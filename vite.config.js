a2e0c7bae8af7d32e4a7676f91e153e9746fedcf
HuynhDangCTA <113507149+HuynhDangCTA@users.noreply.github.com>
2023-04-07 08:31:36 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3001,
+    port: 3000,
+    proxy: {
+      "/api": {
+      target: "http://localhost:3001/",
+      changeOrigin: true,
+      },
+    }      
     },
   plugins: [vue()],
   resolve: {
