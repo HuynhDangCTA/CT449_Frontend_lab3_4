e7912d60bda5250e91e2d808e8a16fb8d47d5708
HuynhDangCTA <113507149+HuynhDangCTA@users.noreply.github.com>
2023-04-07 08:34:24 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -1,32 +1,36 @@
-<script>
-export default {
-props: {
-contact: { type: Object, required: true },
-},
-};
-</script>
 <template>
-<div>
-<div class="p-1">
-<strong>Tên:</strong>
-{{ contact.name }}
-</div>
-<div class="p-1">
-<strong>E-mail:</strong>
-{{ contact.email }}
-</div>
-<div class="p-1">
-<strong>Địa chỉ:</strong>
-{{ contact.address }}
-</div>
-<div class="p-1">
-<strong>Điện thoại:</strong>
-{{ contact.phone }}
-</div>
-<div class="p-1">
-<strong>Liên hệ yêu thích:&nbsp;</strong>
-<i v-if="contact.favorite" class="fas fa-check"></i>
-<i v-else class="fas fa-times"></i>
-</div>
-</div>
-</template>
+    <div>
+      <div class="p-1">
+        <strong>Tên:</strong>
+        {{ contact.name }}
+      </div>
+      <div class="p-1">
+        <strong>E-mail:</strong>
+        {{ contact.email }}
+      </div>
+      <div class="p-1">
+        <strong>Địa chỉ:</strong>
+        {{ contact.address }}
+      </div>
+      <div class="p-1">
+        <strong>Điện thoại:</strong>
+        {{ contact.phone }}
+      </div>
+      <div class="p-1">
+        <strong>Liên hệ yêu thích:</strong>
+        <i :class="{'fas fa-check': contact.favorite, 'fas fa-times': !contact.favorite}"></i>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      contact: {
+        type: Object,
+        required: true
+      }
+    }
+  };
+  </script>
+  
